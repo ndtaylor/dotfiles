475e5317075f5615757e912d2e229d6bbfa178a7
Nathan Taylor <ndtaylor.2012@gmail.com>
2015-04-28 12:09:48 -0700

    Alias for git push

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -10,7 +10,7 @@ fi
 alias gl='git log --first-parent'
 alias glg='git log --graph'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
+alias gp='git push'
 alias gd='git diff'
 alias gc='git commit'
 alias gca='git commit -a'
